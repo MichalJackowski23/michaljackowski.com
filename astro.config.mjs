75fa1ddbb41398a0439ca901bb27f3025e6278b6
MichalJackowski23 <drmjackowski@gmail.com>
2026-05-05 14:27:37 +0200

    po audycie

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://michaljackowski.com',
+  integrations: [
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          pl: 'pl'
+        }
+      }
+    })
+  ],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'pl'],
